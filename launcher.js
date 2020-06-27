4fb4ab4bd5133a1c344ffb6cf529cc7cb287ce93
Shakti <65832818+Shaktinetra@users.noreply.github.com>
2020-06-27 16:22:20 -0400

    Update launcher.js

--- launcher.js
+++ launcher.js
@@ -20,7 +20,7 @@ class Launcher{
             var pointA = this.body.bodyA.position;
             var pointB = this.pointB;
             strokeWeight(4);
-            stroke("black);
+            fill(0);
             line(pointA.x, pointA.y, pointB.x, pointB.y);
         }
     }
