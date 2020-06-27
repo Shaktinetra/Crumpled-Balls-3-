d8981fdd4581b87feda14169b64ff3ea0d40e168
Shakti <65832818+Shaktinetra@users.noreply.github.com>
2020-06-27 16:19:38 -0400

    Update trash.js

--- trash.js
+++ trash.js
@@ -3,9 +3,9 @@ class Trash {
         var options ={
             isStatic: true
         }
-        this.bodyb = Bodies.rectangle(600, 560, 100, 20, options);
-        this.bodyr = Bodies.rectangle(645, 565, 20, 100, options);
-        this.bodyl = Bodies.rectangle(555, 565, 20, 100, options);
+        this.bodyb = Bodies.rectangle(600, 570, 100, 10, options);
+        this.bodyr = Bodies.rectangle(635, 565, 10, 100, options);
+        this.bodyl = Bodies.rectangle(565, 565, 10, 100, options);
         this.image = loadImage("dustbingreen.png");
         World.add(world, this.bodyb);
         World.add(world, this.bodyr);
@@ -14,6 +14,6 @@ class Trash {
 
     display() {
         imageMode(CENTER);
-        image(this.image, 600, 560, 100, 100)
+        image(this.image, 600, 560, 150, 150)
     }
 }
