b6ea0322789caa68a882abea674edfc1cd3ce73d
Shakti <65832818+Shaktinetra@users.noreply.github.com>
2020-07-04 16:40:53 -0400

    Update paper.js

--- paper.js
+++ paper.js
@@ -16,7 +16,7 @@ class Paper {
         push();
         imageMode(CENTER);
         translate(pos.x, pos.y);
-        rotate(angle);
+        rotate(this.body.angle);
         fill(255);
         image(this.image, pos.x, pos.y, 50, 50);
         pop();
