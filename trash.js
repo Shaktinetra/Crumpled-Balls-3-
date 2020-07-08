2009d052572ae6da984716bc1bdde68a1a06bd95
Shakti <65832818+Shaktinetra@users.noreply.github.com>
2020-07-08 12:37:47 -0400

    Update trash.js

--- trash.js
+++ trash.js
@@ -13,10 +13,7 @@ class Trash {
     }
 
     display() {
-        rectMode(CENTER);
-       //image(this.image, 600, 500, 200, 200)
-        rect(600, 500, 180, 10);
-        rect(690, 500, 10, 200);
-        rect(510, 500, 10, 200);
+        imageMode(CENTER);
+        image(this.image, 600, 500, 200, 200)
     }
 }
