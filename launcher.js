98235edc117c43e7c962821cd4b12bcdf9412b73
Shakti <65832818+Shaktinetra@users.noreply.github.com>
2020-07-12 11:45:33 -0400

    Update launcher.js

--- launcher.js
+++ launcher.js
@@ -4,7 +4,7 @@ class Launcher{
             bodyA: bodyA,
             pointB: pointB,
             stiffness: 1.0,
-            length: 30
+            length: 35
         }
         this.pointB = pointB;
         this.body = Constraint.create(options);
